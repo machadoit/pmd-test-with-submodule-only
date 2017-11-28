769dbc7d2925b5646ddae2822373659b7f507031
Joao Machado <machado@codacy.com>
2017-11-28 11:16:26 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,12 +2,11 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("Hello, World!");
         try {
             int i = 1 / 0;
             System.out.println(i);
         } catch (Exception e) {
-
         }
     }
 
